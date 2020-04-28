348060356d799c2d3ad54823c9fc6128dc288485
neilellis <hello@neilellis.me>
2020-04-28 16:27:13 +0100

    Build changes.

--- bin/cypress.sh
+++ bin/cypress.sh
@@ -1,3 +1,6 @@
+# Script to run the Cypress UI for testing
+# Make sure you set the environment variables:
+# TEST_AC_USER (test a/c username) and TEST_AC_PASS (test a/c password)
 cd $(dirname $0)
 cd ..
-node ./node_modules/cypress/bin/cypress open -e TEST_AC_USER=${TEST_AC_USER},TEST_AC_PASS=${TEST_AC_PASS} 
+node ./node_modules/cypress/bin/cypress open -e TEST_AC_USER=${TEST_AC_USER},TEST_AC_PASS=${TEST_AC_PASS}
--- bin/delete-branch.sh
+++ bin/delete-branch.sh
@@ -1,1 +1,5 @@
+# Use this script to remove no-longer used feature branch environments
+# Amplify creates a feature branch environment for every feature/xxx branch
+# In github. Run this script to remove them when no longer needed.
+# Passsing the xxx value as the first and only argument to the script
 aws amplify delete-branch --app-id dtmxl3q3i7oix --branch-name "feature/$1" --profile socialsensing
