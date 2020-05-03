d9c057c1f0f50ba0eddbe8b512c4dd217a0669b6
neilellis <hello@neilellis.me>
2020-05-03 22:48:55 +0100

    Issue: https://github.com/socialsensingbot/frontend/issues/77 Issue Description: If all tweets are missing spinner stays

---
+++ cypress/integration/00077_all_missing_tweets_spec.js
@@ -0,0 +1,25 @@
+describe('Missing tweets: ', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:4200/map');
+    cy.stubLiveJson("live-03-05-2020");
+    cy.login();
+  });
+
+  describe('select county with only missing tweets', () => {
+    console.debug("Tests https://github.com/socialsensingbot/frontend/issues/67");
+    const url = "http://localhost:4200/map?min_offset=-1460&max_offset=-20&selected=roscommon";
+    it('should say how many tweets there are in total', () => {
+      cy.visit(url);
+      cy.url().should("equal", url);
+      cy.noSpinner();
+      cy.url().should("equal", url);
+      cy.get(".tweet-drawer", {timeout: 20000}).should("be.visible");
+      cy.twitterPanelHeader("Showing 2 of 2 Tweets from Roscommon");
+      cy.noSpinner();
+      cy.logout();
+    });
+  });
+
+
+
+});
