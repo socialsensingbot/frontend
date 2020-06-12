8343065865058a540ebcc049a02effbfdb8e31e5
neilellis <hello@neilellis.me>
2020-06-12 11:05:37 +0100

    Issue: https://github.com/socialsensingbot/frontend/issues/83 Issue Description: Export tweets function Stripped html tags from tweet in CSV

--- src/app/map/twitter/tweet.ts
+++ src/app/map/twitter/tweet.ts
@@ -145,7 +145,7 @@ export class Tweet {
 
   public asCSV(): CSVExportTweet {
     this.lazyInit();
-    return new CSVExportTweet(this._id, this._date, this._url, this._html);
+    return new CSVExportTweet(this._id, this._date, this._url, this._html.replace(/<[^>]+>/g, ""));
   }
 }
 
