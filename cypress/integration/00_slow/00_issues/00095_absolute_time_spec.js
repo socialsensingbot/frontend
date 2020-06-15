5e41d6c21ee2e17683b4162b5e7f0d8b8beefa85
neilellis <hello@neilellis.me>
2020-06-15 18:05:57 +0100

    Issue: https://github.com/socialsensingbot/frontend/issues/95 Issue Description: URL should store absolute not relative min max times Moved spec as it is long(ish) running.

--- cypress/integration/00_slow/00_issues/00095_absolute_time_spec.js
+++ cypress/integration/00_slow/00_issues/00095_absolute_time_spec.js
@@ -1,4 +1,12 @@
 const zoomDuration = 1000;
+
+function confirmSlider() {
+  cy.get(".slider-date-time-min .slider-date", {timeout: 20000}).should("contain.text", "24-Apr-20");
+  cy.get(".slider-date-time-min .slider-time").should("contain.text", "6 PM");
+  cy.get(".slider-date-time-max .slider-date", {timeout: 20000}).should("contain.text", "25-Apr-20");
+  cy.get(".slider-date-time-max .slider-time").should("contain.text", "6 PM");
+}
+
 describe('URL State: ', function () {
   beforeEach(() => {
   })
@@ -15,29 +23,20 @@ describe('URL State: ', function () {
         "The URL should auto update based on the latest data's latest time, adjusting the min and max offsets accordingly.");
       cy.url({timeout: 20000}).should("equal",
                                       "http://localhost:4200/map?selected=powys&min_offset=-3109&max_offset=-1669&abs_time=1587936900000");
-      cy.get(".slider-date-time-min .slider-date", {timeout: 20000}).should("contain.text", "24-Apr-20");
-      cy.get(".slider-date-time-min .slider-time").should("contain.text", "6 PM");
-      cy.get(".slider-date-time-max .slider-date", {timeout: 20000}).should("contain.text", "25-Apr-20");
-      cy.get(".slider-date-time-max .slider-time").should("contain.text", "6 PM");
+      confirmSlider();
       cy.get(".app-tweet-drawer", {timeout: 20000}).should("be.visible");
       cy.twitterPanelHeader("Powys");
-      cy.stubLiveJson("live-long");
       cy.log(
         "We now wait for a data update to occur, the date values on the slider *must not* change. But the URL will be adjusted to the new absolute time.")
+      cy.stubLiveJson("live-long");
       cy.wait(65000)
       cy.url({timeout: 20000}).should("equal",
                                       "http://localhost:4200/map?selected=powys&min_offset=-3589&max_offset=-2149&abs_time=1588009140000");
-      cy.get(".slider-date-time-min .slider-date", {timeout: 20000}).should("contain.text", "24-Apr-20");
-      cy.get(".slider-date-time-min .slider-time").should("contain.text", "6 PM");
-      cy.get(".slider-date-time-max .slider-date", {timeout: 20000}).should("contain.text", "25-Apr-20");
-      cy.get(".slider-date-time-max .slider-time").should("contain.text", "6 PM");
-      cy.reload();
+      confirmSlider();
       cy.log("We confirm the URL has the right information in it by reloading the page.")
+      cy.reload();
       cy.get(".slider-date-time", {timeout: 20000});
-      cy.get(".slider-date-time-min .slider-date", {timeout: 20000}).should("contain.text", "24-Apr-20");
-      cy.get(".slider-date-time-min .slider-time").should("contain.text", "6 PM");
-      cy.get(".slider-date-time-max .slider-date", {timeout: 20000}).should("contain.text", "25-Apr-20");
-      cy.get(".slider-date-time-max .slider-time").should("contain.text", "6 PM");
+      confirmSlider();
 
       cy.logout();
     });
