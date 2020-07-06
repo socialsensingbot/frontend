747d2ae7d01921b8238312d5110688e0d6254b55
neilellis <hello@neilellis.me>
2020-07-06 15:00:17 +0100

    Demo environment

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production:       true,
+  name:             "demo",
+  version:          "demo",
+  // timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,
+  timezone:         "UTC",
+  locale:           'en-GB',
+  rollbar:          true,
+  multipleSessions: false,
+  hmr:              false,
+  confirm:          {
+    email:    '',
+    password: ''
+  }
+};
