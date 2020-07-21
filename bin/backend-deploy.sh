49216bf48d79b83149721c3446790d990be84549
neilellis <hello@neilellis.me>
2020-07-21 20:29:30 +0100

    jq

--- bin/backend-deploy.sh
+++ bin/backend-deploy.sh
@@ -1,18 +1,18 @@
 # Script to run the for the Amplify Console CI Cypress testing
 cd $(dirname $0)
 cd ..
-export PATH=PATH:$(dirname $0)
+export PATH=$PATH:$(dirname $0)
 
 if [[ "${AWS_BRANCH}" == staging ]]; then
-  ./backup.sh
+  .backup.sh
   amplifyPush --simple
 elif [[ "${AWS_BRANCH}" == demo ]]; then
-  ./backup.sh
+  backup.sh
   amplifyPush --simple
 elif [[ "${AWS_BRANCH}" == master ]]; then
   echo "NOTE: No backend changes should me made on master, all must pass through staging. So not pushing backend."
 elif [[ "${AWS_BRANCH}" == release* ]]; then
-  ./backup.sh
+  backup.sh
   amplifyPush --simple
 else
   amplifyPush --simple
