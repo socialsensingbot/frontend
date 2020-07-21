a8b9e2e5f1ce2249f0b73864b699efffab940357
neilellis <hello@neilellis.me>
2020-07-21 18:52:39 +0100

    Adding backups to deployment process.

---
+++ bin/backend-deploy.sh
@@ -0,0 +1,18 @@
+# Script to run the for the Amplify Console CI Cypress testing
+cd $(dirname $0)
+cd ..
+
+if [[ "${AWS_BRANCH}" == staging ]]; then
+  ./backup.sh
+  amplifyPush --simple
+elif [[ "${AWS_BRANCH}" == demo ]]; then
+  ./backup.sh
+  amplifyPush --simple
+elif [[ "${AWS_BRANCH}" == master ]]; then
+  echo "NOTE: No backend changes should me made on master, all must pass through staging. So not pushing backend."
+elif [[ "${AWS_BRANCH}" == release* ]]; then
+  ./backup.sh
+  amplifyPush --simple
+else
+  amplifyPush --simple
+fi
---
+++ bin/backup.sh
@@ -0,0 +1,18 @@
+set -euxo pipefail
+cd $(dirname $0)
+cd ..
+
+echo "Backing up environments"
+npm install -g cognito-backup
+cognito-backup backup-all-users eu-west-2_dkJC8ZcOU --region eu-west-2 --profile socialsensing
+for file in eu-west-2*.json
+do
+  aws s3 cp $file s3://socialsensing-backups/cognito/$file --profile socialsensing
+  rm $file
+done
+
+for table in $(aws dynamodb list-tables --profile socialsensing | jq -r .TableNames[])
+do
+  aws dynamodb scan --profile socialsensing --table-name $table --output json > /tmp/table-dump.json
+  aws s3 cp  /tmp/table-dump.json s3://socialsensing-backups/dynamodb/$table.json --profile socialsensing
+done
