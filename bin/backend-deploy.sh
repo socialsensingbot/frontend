f8766dcd938d2651010e569c0bd2670ef9ea5ac3
neilellis <hello@neilellis.me>
2020-07-21 21:14:58 +0100

    Remove untracked files.

--- bin/backend-deploy.sh
+++ bin/backend-deploy.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 cd $(dirname $0)
 export PATH=$PATH:$(pwd)
 cd ..
+cd amplify && git clean -fdx && cd ..
 
 if [[ "${AWS_BRANCH}" == staging ]]; then
   backup.sh
@@ -14,9 +15,9 @@ elif [[ "${AWS_BRANCH}" == master ]]; then
   echo "NOTE: No backend changes should me made on master, all must pass through staging. So not pushing backend."
 elif [[ "${AWS_BRANCH}" == release* ]]; then
 #  backup.sh
-  cd amplify && git clean -fdx && cd ..
-  amplify env checkout dev
-  yes | amplify push
+  amplify init --amplify --yes
+  amplify env checkout dev --yes
+  amplify push --yes
 else
   amplifyPush --simple
 fi
