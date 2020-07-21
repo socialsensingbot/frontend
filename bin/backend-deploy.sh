f29a353fdc55ba3a08735e175c156f9a81c5c2b2
neilellis <hello@neilellis.me>
2020-07-21 21:24:20 +0100

    Remove untracked files.

--- bin/backend-deploy.sh
+++ bin/backend-deploy.sh
@@ -1,23 +1,30 @@
 # Script to run the for the Amplify Console Backend Deployment
 set -euxo pipefail
-cd $(dirname $0)
+cd "$(dirname $0)"
 export PATH=$PATH:$(pwd)
 cd ..
-cd amplify && git clean -fdx && cd ..
+#cd amplify && git clean -fdx && cd ..
+
+amplify_push() {
+  pwd
+  amplify init --yes
+  amplify env checkout $1 --yes
+  amplify push --yes
+}
 
 if [[ "${AWS_BRANCH}" == staging ]]; then
   backup.sh
-  amplifyPush --simple
+  amplify_push prod
 elif [[ "${AWS_BRANCH}" == demo ]]; then
   backup.sh
-  amplifyPush --simple
+  amplify_push demo
 elif [[ "${AWS_BRANCH}" == master ]]; then
+  amplify init --yes
+  amplify env checkout prod --yes
   echo "NOTE: No backend changes should me made on master, all must pass through staging. So not pushing backend."
 elif [[ "${AWS_BRANCH}" == release* ]]; then
 #  backup.sh
-  amplify init --yes
-  amplify env checkout dev --yes
-  amplify push --yes
+  amplify_push dev
 else
-  amplifyPush --simple
+  amplify_push dev
 fi
