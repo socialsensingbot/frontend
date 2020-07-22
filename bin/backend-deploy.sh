c3a479c304065423f4112d68d6fff74870a26e90
neilellis <hello@neilellis.me>
2020-07-22 01:25:05 +0100

    Reverted to using amplifyPush

--- bin/backend-deploy.sh
+++ bin/backend-deploy.sh
@@ -6,50 +6,17 @@ cd ..
 #cd amplify && git clean -fdx && cd ..
 
 
-
-amplify_push() {
-  ENV=$1
-AMPLIFY="{\
-\"envName\":\"${ENV}\",\
-\"appId\":\"${AWS_APP_ID}\"\
-}"
-
-AWSCONFIG="{\
-\"configLevel\":\"project\",\
-\"useProfile\":true,\
-\"profileName\":\"default\",\
-\"AmplifyAppId\":\"${AWS_APP_ID}\"\
-}"
-
-PROVIDERS="{\
-\"awscloudformation\":${AWSCONFIG}\
-}"
-
-CODEGEN="{\
-\"generateCode\":true,\
-\"generateDocs\":false\
-}"
-
-  pwd
-  amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes;
-  amplify env checkout ${ENV}
-  amplify codegen
-  amplify push --yes
-}
-
 if [[ "${AWS_BRANCH}" == staging ]]; then
   backup.sh
-  amplify_push prod
+  amplifyPush --simple
 elif [[ "${AWS_BRANCH}" == demo ]]; then
   backup.sh
-  amplify_push demo
+  amplifyPush --simple
 elif [[ "${AWS_BRANCH}" == master ]]; then
-  amplify init --yes
-  amplify env checkout prod --yes
   echo "NOTE: No backend changes should me made on master, all must pass through staging. So not pushing backend."
 elif [[ "${AWS_BRANCH}" == release* ]]; then
 #  backup.sh
-  amplify_push dev
+  amplifyPush --simple
 else
-  amplify_push dev
+  amplifyPush --simple
 fi
