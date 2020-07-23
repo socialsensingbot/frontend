8d64092f5e8ec3b98bc0ba862a17cf79b51fc70a
neilellis <hello@neilellis.me>
2020-07-23 21:24:52 +0100

    Branch for Release 1.3.4 created.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production:           true,
   name:                 "production",
   demo:                 false,
-  version:              "1.3.2",
+  version: "1.3.4",
   // timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,
   timezone:             "UTC",
   locale:               "en-GB",
