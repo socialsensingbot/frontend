262a6d92f904471f4f22bfcf187817158f78fc23
neilellis <hello@neilellis.me>
2021-02-04 15:20:33 +0000

    Fixed invalid values in CSV columns, also closes #160

--- src/app/map/twitter/tweet.ts
+++ src/app/map/twitter/tweet.ts
@@ -131,13 +131,13 @@ export class Tweet {
     let source = "";
     this.lazyInit();
     if (sanitize) {
-      return new CSVExportTweet(regionMap[this._place], this._id, this._date.toUTCString(),
+      return new CSVExportTweet(regionMap[this._place], impact, source, this._id, this._date.toUTCString(),
                                 "https://twitter.com/username_removed/status/" + this._id,
-                                this.sanitizeForGDPR($("<div>").html(this._html).text()), impact, source);
+                                this.sanitizeForGDPR($("<div>").html(this._html).text()));
 
     } else {
-      return new CSVExportTweet(regionMap[this._place], this._id, this._date.toUTCString(), this._url,
-                                $("<div>").html(this._html).text(), impact, source);
+      return new CSVExportTweet(regionMap[this._place], impact, source, this._id, this._date.toUTCString(), this._url,
+                                $("<div>").html(this._html).text());
     }
   }
 
