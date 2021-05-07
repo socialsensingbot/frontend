86797023391f455a83458c8ccb6b750d97147cbb
neilellis <hello@neilellis.me>
2021-05-07 18:38:05 +0100

    Fixing test for #175 & #164

--- cypress/integration/00_quick/03_ignore_pref_spec.js
+++ cypress/integration/00_quick/03_ignore_pref_spec.js
@@ -7,7 +7,9 @@ describe('03 Ignore tweets: ', function () {
     cy.stubLiveJson("live-old");
   })
 
-  const menu2ndOpt = "body .mat-menu-item:nth-child(2)";
+  const markAsIgnoredMenu = "body .mat-menu-item.tweet-menu-ignore-tweet";
+  const markAsUnignoredMenu = "body .mat-menu-item.tweet-menu-unignore-tweet";
+  const markAsMenu = "body .mat-menu-item.tweet-list-item-menu-mark-as";
   const url = MAP_URL + "?selected=carmarthenshire&max_offset=0&min_offset=-1439";
   const test = () => {
     cy.visit(url);
@@ -23,23 +25,30 @@ describe('03 Ignore tweets: ', function () {
         cy.get(tweetVisible + " .app-tweet-item", {timeout: 60000});
         cy.get(tweetVisible + " .app-tweet-item", {timeout: 60000}).should('be.visible');
         cy.get(tweetVisible + " .mat-icon", {timeout: 60000}).click({force: true});
-        cy.get(menu2ndOpt).click();
+        cy.get(markAsMenu).click();
+        cy.wait(1000);
+        cy.get(markAsIgnoredMenu).click();
         cy.get(".mat-tab-label:nth-child(2)", {timeout: 30000}).click();
         cy.wait(4000);
         cy.get(tweetHidden, {timeout: 60000});
         cy.get(tweetHidden + " .app-tweet-item", {timeout: 60000});
         cy.get(tweetHidden + " .mat-icon", {timeout: 30000}).click({force: true});
-        cy.get(menu2ndOpt).contains("Unignore Tweet");
+        cy.get(markAsMenu).click();
+        cy.wait(1000);
+        cy.get(markAsUnignoredMenu).contains("Unignored Tweet");
       } else {
         cy.get(".mat-tab-label:nth-child(2)", {timeout: 30000}).click({force: true});
         cy.get(tweetHidden + " .mat-icon", {timeout: 60000}).click({force: true});
-        cy.get(menu2ndOpt).click();
+        cy.get(markAsMenu).click();
+        cy.wait(1000);
+        cy.get(markAsUnignoredMenu).click();
         cy.wait(4000);
         cy.get(".mat-tab-label:nth-child(1)", {timeout: 30000}).click({force: true});
         cy.get(tweetVisible + " .mat-icon", {timeout: 30000}).click({force: true});
-        cy.get(menu2ndOpt).contains("Ignore Tweet");
+        cy.get(markAsMenu).click();
+        cy.wait(1000);
+        cy.get(markAsIgnoredMenu).contains("Ignored Tweet");
       }
-
     });
   };
   it('toggle tweet', test);
