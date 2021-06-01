6801d91729efe5c614bba7e71539cd6f84949f4d
neilellis <hello@neilellis.me>
2021-06-01 16:10:48 +0100

    Branch for Release 2.0 created.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production:           true,
   name:                 "production",
   demo:                 false,
-  version:              "1.3.2",
+  version: "2.0",
   // timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,
   timezone:             "UTC",
   locale:               "en-GB",
