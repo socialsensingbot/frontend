68fff8d2ce9063f0625c15b72365440137a6a9d9
Neil Ellis <neilellis@users.noreply.github.com>
2021-06-29 19:57:05 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -47,5 +47,5 @@ export const environment = {
   recentTweetHighlightOffsetInSeconds: 1200,
 
   mapTileUrlTemplate:   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnVkeWFydGh1ciIsImEiOiJjamZrem1ic3owY3k4MnhuYWt2dGxmZmk5In0.ddp6_hNhs_n9MJMrlBwTVg"
-
+  blinkRateInMilliseconds: 400,
 };
