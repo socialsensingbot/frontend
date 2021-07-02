9e7017fb4ce31d5c3ec06b95d4837227238cfef8
neilellis <hello@neilellis.me>
2021-07-03 00:13:45 +0100

    Fixed Dashboard reset problems by telling AWS that the dashboard is a string not JSON.

---
+++ amplify/backend/function/query/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.handler = void 0;
+var awsServerlessExpress = require('aws-serverless-express');
+var server = awsServerlessExpress.createServer(require('./app'));
+exports.handler = function (event, context) {
+    console.log("EVENT: " + JSON.stringify(event));
+    return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,58 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  name:         "dev",
-  version:      "dev",
-  demo:         false,
-  production:   false,
-  hmr:          false,
-  rollbar:      false,
-  toolbarColor: "primary",
-  // timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  timezone:         "UTC",
-  multipleSessions: true, //Can the user be logged into multiple devices/browsers at once?
-  maxUsers:         -1, // can be -1 (no limit), 0 - no logins, 1 - single user at a time, n - n concurrent users.
-  locale:           "en-GB",
-  confirm:          {
-    email:    "",
-    password: ""
-  },
-
-  impact:               {
-    //The ability to tag tweets with an impact annotation
-    levels: [
-      {title: "1 – Minimal", value: "minimal", color: "#43A047"},
-      {title: "2 – Minor", value: "minor", color: "#FFEE58"},
-      {title: "3 - Significant", value: "significant", color: "#FFB300"},
-      {title: "4 - Severe", value: "severe", color: "#F4511E"}
-    ]
-  },
-  source:               {
-    //The ability to tag tweets with a source
-    types: [
-      {title: "River", value: "river", color: "#43A047"},
-      {title: "Surface", value: "surface", color: "#FFEE58"},
-      {title: "Groundwater", value: "groundwater", color: "#FFB300"},
-      {title: "Coastal", value: "coastal", color: "#F4511E"}
-    ]
-  },
-  features:             ["impact", "source"],
-  showErrors:           false,
-  showLoadingMessages:  true,
-  mostRecentDateIsNow:  true,
-  sanitizeForGDPR:      true,
-  continuousUpdateThresholdInMinutes: 10,
-  animateOnTimeSliderChange: false,
-  recentTweetHighlightOffsetInSeconds: 1200,
-  defaultDataSet:       "uk-flood-live",
-  availableDataSets:    ["*"], // A list of datasets that will appear in the toolbar dropdown, or "*" for all.
-  cacheProcessedTweets: false,
-  mapTileUrlTemplate:   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnVkeWFydGh1ciIsImEiOiJjamZrem1ic3owY3k4MnhuYWt2dGxmZmk5In0.ddp6_hNhs_n9MJMrlBwTVg",
-  // mapTileUrlTemplate:   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicnVkeWFydGh1ciIsImEiOiJjamZrem1ic3owY3k4MnhuYWt2dGxmZmk5In0.ddp6_hNhs_n9MJMrlBwTVg"
-  blinkRateInMilliseconds: 400,
-};
-
+import {Dashboard} from "../app/pref/dashboard.service";
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -62,3 +11,80 @@ export const environment = {
  * on performance if an error is thrown.
  */
 import "zone.js/dist/zone-error"; // Included with Angular CLI.
+import {v4 as uuidv4} from "uuid";
+
+const defaultDashboard: Dashboard = {
+    devices: [{
+        deviceType: "all",
+        pages:      [{
+            title: "First Page",
+            cards: [
+                {title:    "Count By Text and Region",
+                    cols:  2,
+                    rows:  2,
+                    type:  "timeseries-text-and-region",
+                    state: {},
+                    id:    uuidv4()
+                },
+                {title: "Total Count", cols: 1, rows: 1, type: "timeseries-total", state: {}, id: uuidv4()},
+                // {title: "Exceedence by Date", cols: 1, rows: 1, type: "timeseries-exceed", state: {}},
+                // {title: "Exceedence by Region", cols: 2, rows: 1, type: "timeseries-exceed-region", state: {}},
+            ]
+        }]
+    }]
+};
+export const environment = {
+    name:             "dev",
+    lamdaEnvironment: "dev",
+    version:          "dev",
+    demo:             false,
+    production:       false,
+    hmr:              false,
+    rollbar:          false,
+    toolbarColor:     "primary",
+    // timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    timezone:         "UTC",
+    multipleSessions: true, // Can the user be logged into multiple devices/browsers at once?
+    maxUsers:         -1, // can be -1 (no limit), 0 - no logins, 1 - single user at a time, n - n concurrent users.
+    locale:           "en-GB",
+    confirm:          {
+        email:    "",
+        password: ""
+    },
+
+    impact:                              {
+        // The ability to tag tweets with an impact annotation
+        levels: [
+            {title: "1 – Minimal", value: "minimal", color: "#43A047"},
+            {title: "2 – Minor", value: "minor", color: "#FFEE58"},
+            {title: "3 - Significant", value: "significant", color: "#FFB300"},
+            {title: "4 - Severe", value: "severe", color: "#F4511E"}
+        ]
+    },
+    source:                              {
+        // The ability to tag tweets with a source
+        types: [
+            {title: "River", value: "river", color: "#43A047"},
+            {title: "Surface", value: "surface", color: "#FFEE58"},
+            {title: "Groundwater", value: "groundwater", color: "#FFB300"},
+            {title: "Coastal", value: "coastal", color: "#F4511E"}
+        ]
+    },
+    features:                            ["impact", "source"],
+    showErrors:                          false,
+    showLoadingMessages:                 true,
+    mostRecentDateIsNow:                 true,
+    sanitizeForGDPR:                     true,
+    continuousUpdateThresholdInMinutes:  5,
+    animateOnTimeSliderChange:           false,
+    recentTweetHighlightOffsetInSeconds: 1200,
+    defaultDataSet:                      "uk-flood-live",
+    availableDataSets:                   ["*"], // A list of datasets that will appear in the toolbar dropdown, or "*" for all.
+    cacheProcessedTweets:                false,
+    mapTileUrlTemplate:                  "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnVkeWFydGh1ciIsImEiOiJjamZrem1ic3owY3k4MnhuYWt2dGxmZmk5In0.ddp6_hNhs_n9MJMrlBwTVg",
+    defaultDashboard:                    defaultDashboard,
+    blinkRateInMilliseconds:             1000,
+    // mapTileUrlTemplate:   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicnVkeWFydGh1ciIsImEiOiJjamZrem1ic3owY3k4MnhuYWt2dGxmZmk5In0.ddp6_hNhs_n9MJMrlBwTVg"
+
+};
+
