c99f6e682a71a08b67b1ba0f3701b6d08f37a86a
neilellis <hello@neilellis.me>
2021-07-08 15:41:45 +0100

    Boolean full text : #227

---
+++ amplify/backend/function/query/src/queries.ts
@@ -0,0 +1,90 @@
+/*
+ * © 2020 All rights reserved.
+ */
+
+import {QueryOptions} from "mysql";
+
+const dateFromMillis = (time: number) => {
+    const dateTime = new Date(time);
+    return new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate());
+};
+
+
+export const queries: { [id: string]: (params) => QueryOptions } = {
+    count_by_date_for_regions:              (params: any) => {
+        if (!params.regions || (params.regions.includes("*") || params.regions.length === 0)) {
+            return {
+                sql: `SELECT sum(message_count) as count, aggregate_date as date, avg(exceedence) as exceedence, 'all' as region
+                      FROM aggregate_counts_by_region
+                      WHERE source = ?
+                        and hazard = ?
+                        and aggregate_date between ? and ?
+                      group by aggregate_date
+                      order by aggregate_date`,
+                values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to)]
+            };
+        } else {
+            return {
+                sql: `SELECT message_count as count, aggregate_date as date, exceedence, region
+                      FROM aggregate_counts_by_region
+                      WHERE source = ?
+                        and hazard = ?
+                        and aggregate_date between ? and ?
+                        and region in (?)
+                      order by aggregate_date`,
+                values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to),
+                         params.regions]
+            };
+        }
+    },
+    count_by_date_for_regions_and_fulltext: (params: any) => {
+        let fullText = "";
+        if (typeof params.textSearch !== "undefined" && params.textSearch.length > 0) {
+            fullText = " and MATCH (source_text) AGAINST(? IN BOOLEAN MODE) ";
+        }
+        if( !params.regions || (params.regions.includes("*") || params.regions.length === 0)) {
+            return {
+                sql: `SELECT count(*) as count, source_date as date, 'all' as region_1, '' as region_2, '' as region_3
+                      FROM text_by_region
+                      WHERE source = ?
+                        and hazard = ?
+                        and source_date between ? and ? ${fullText}
+                      group by source_date
+                      order by source_date`,
+                values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to),
+                         params.textSearch]
+            };
+        } else {
+            return {
+                sql: `SELECT count(*) as count, source_date as date, region_1, region_2, region_3
+                      FROM text_by_region
+                      WHERE source = ?
+                        and hazard = ?
+                        and source_date between ? and ?
+                        and region_1 in (?)
+                          ${fullText}
+                      group by source_date, region_1, region_2, region_3
+                      order by source_date`,
+                values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to),
+                         params.regions, params.textSearch]
+            };
+
+        }
+    },
+    count_by_date_for_all_regions:          (params: any) => {
+
+        return {
+            sql: `SELECT sum(message_count) as count, aggregate_date as date, avg(exceedence) as exceedence, 'all' as region
+                  FROM aggregate_counts_by_region
+                  WHERE source = ?
+                    and hazard = ?
+                    and aggregate_date between ? and ?
+                  group by aggregate_date
+                  order by aggregate_date`,
+            values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to)]
+        };
+
+    }
+
+};
+
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,20 +3,20 @@
 
 const awsmobile = {
     "aws_project_region": "eu-west-2",
-    "aws_cognito_identity_pool_id": "eu-west-2:6b40e002-0911-41ed-a890-b9aa372889ff",
+    "aws_cognito_identity_pool_id": "eu-west-2:6ccfa30d-2c16-4cff-9bad-99c52159ad53",
     "aws_cognito_region": "eu-west-2",
-    "aws_user_pools_id": "eu-west-2_N8xKBZO9M",
-    "aws_user_pools_web_client_id": "5n2v2fs3gf7ns381u3e6v39i7b",
+    "aws_user_pools_id": "eu-west-2_L3RQGANbS",
+    "aws_user_pools_web_client_id": "77ie34pv0ck1k59emrq4jq3k4m",
     "oauth": {},
-    "aws_user_files_s3_bucket": "json155623-pwsdemo",
+    "aws_user_files_s3_bucket": "json183906-dev",
     "aws_user_files_s3_bucket_region": "eu-west-2",
-    "aws_appsync_graphqlEndpoint": "https://6q6otv363je3rdhzxpuixuqkum.appsync-api.eu-west-2.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://bjqdpnjuyrhi7loznimshzuhhy.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
     "aws_cloud_logic_custom": [
         {
             "name": "query",
-            "endpoint": "https://i93keky545.execute-api.eu-west-2.amazonaws.com/pwsdemo",
+            "endpoint": "https://napghm5qba.execute-api.eu-west-2.amazonaws.com/dev",
             "region": "eu-west-2"
         }
     ]
