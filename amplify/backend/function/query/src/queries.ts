b067805570f60528eb603318da64d3ba241f4e1f
neilellis <hello@neilellis.me>
2021-07-09 17:19:04 +0100

    Regions #228

--- amplify/backend/function/query/src/queries.ts
+++ amplify/backend/function/query/src/queries.ts
@@ -14,7 +14,10 @@ export const queries: { [id: string]: (params) => QueryOptions } = {
     count_by_date_for_regions:              (params: any) => {
         if (!params.regions || (params.regions.includes("*") || params.regions.length === 0)) {
             return {
-                sql: `SELECT sum(message_count) as count, aggregate_date as date, avg(exceedence) as exceedence, 'all' as region
+                sql: `SELECT sum(message_count) as count,
+                             aggregate_date     as date,
+                             avg(exceedence)    as exceedence,
+                             'all'              as region
                       FROM aggregate_counts_by_region
                       WHERE source = ?
                         and hazard = ?
@@ -25,14 +28,17 @@ export const queries: { [id: string]: (params) => QueryOptions } = {
             };
         } else {
             return {
+                // todo - broken see next query
                 sql: `SELECT message_count as count, aggregate_date as date, exceedence, region
                       FROM aggregate_counts_by_region
                       WHERE source = ?
                         and hazard = ?
                         and aggregate_date between ? and ?
-                        and region in (?)
+
+                        and region in (select region from ref_region_groups where parent in (?) or region in (?))
                       order by aggregate_date`,
                 values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to),
+                         params.regions,
                          params.regions]
             };
         }
@@ -42,9 +48,9 @@ export const queries: { [id: string]: (params) => QueryOptions } = {
         if (typeof params.textSearch !== "undefined" && params.textSearch.length > 0) {
             fullText = " and MATCH (source_text) AGAINST(? IN BOOLEAN MODE) ";
         }
-        if( !params.regions || (params.regions.includes("*") || params.regions.length === 0)) {
+        if (!params.regions || (params.regions.includes("*") || params.regions.length === 0)) {
             return {
-                sql: `SELECT count(*) as count, source_date as date, 'all' as region_1, '' as region_2, '' as region_3
+                sql: `SELECT count(*) as count, source_date as date, 'all' as region
                       FROM text_by_region
                       WHERE source = ?
                         and hazard = ?
@@ -56,14 +62,16 @@ export const queries: { [id: string]: (params) => QueryOptions } = {
             };
         } else {
             return {
-                sql: `SELECT count(*) as count, source_date as date, region_1, region_2, region_3
-                      FROM text_by_region
-                      WHERE source = ?
-                        and hazard = ?
-                        and source_date between ? and ?
-                        and region_1 in (?)
+                sql: `SELECT count(*) as count, source_date as date, parent as region
+                      FROM text_by_region tbr,
+                           ref_region_groups as rrg
+                      WHERE tbr.source = ?
+                        and tbr.hazard = ?
+                        and tbr.region_1 = rrg.region
+                        and tbr.source_date between ? and ?
+                        and rrg.parent in (?)
                           ${fullText}
-                      group by source_date, region_1, region_2, region_3
+                      group by source_date, rrg.parent
                       order by source_date`,
                 values: [params.source, params.hazard, dateFromMillis(params.from), dateFromMillis(params.to),
                          params.regions, params.textSearch]
@@ -74,7 +82,10 @@ export const queries: { [id: string]: (params) => QueryOptions } = {
     count_by_date_for_all_regions:          (params: any) => {
 
         return {
-            sql: `SELECT sum(message_count) as count, aggregate_date as date, avg(exceedence) as exceedence, 'all' as region
+            sql: `SELECT sum(message_count) as count,
+                         aggregate_date     as date,
+                         avg(exceedence)    as exceedence,
+                         'all'              as region
                   FROM aggregate_counts_by_region
                   WHERE source = ?
                     and hazard = ?
