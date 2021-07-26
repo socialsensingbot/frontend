3caf67dc19fbcd656b985d60a1eb8fab39c7ee61
neilellis <hello@neilellis.me>
2021-07-26 15:42:17 +0100

    Reformatted tweet list so the menu is visible!

--- amplify/backend/function/query/src/index.js
+++ amplify/backend/function/query/src/index.js
@@ -1,9 +1,10 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 var awsServerlessExpress = require("aws-serverless-express");
 var server = awsServerlessExpress.createServer(require("./app"));
-exports.handler = function (event, context) {
+var handler = function (event, context) {
     console.log("EVENT DATA: " + JSON.stringify(event));
     return awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
 };
+exports.handler = handler;
