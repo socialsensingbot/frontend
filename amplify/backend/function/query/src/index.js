183186318f1c274473df8c8fea1c50615b965be3
neilellis <hello@neilellis.me>
2021-08-25 11:56:40 +0100

    Missing files.

--- amplify/backend/function/query/src/index.js
+++ amplify/backend/function/query/src/index.js
@@ -3,7 +3,8 @@ exports.__esModule = true;
 exports.handler = void 0;
 var awsServerlessExpress = require("aws-serverless-express");
 var server = awsServerlessExpress.createServer(require("./app"));
-exports.handler = function (event, context) {
+var handler = function (event, context) {
     console.log("EVENT DATA: " + JSON.stringify(event));
     return awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
 };
+exports.handler = handler;
