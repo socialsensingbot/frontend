0fcc05eced3f6c6f65d7c1e5d8882f6cd51b7bc4
neilellis <hello@neilellis.me>
2021-09-27 16:12:58 +0100

    Better deleted tweet handling.

--- sql/refresh_mv_now.sql
+++ sql/refresh_mv_now.sql
@@ -26,27 +26,28 @@ BEGIN
            t.source_timestamp,
            tr.region_type,
            tr.region,
-           t.warning
+           t.warning,
+           IFNULL(t.deleted, false)
     FROM live_text t,
          live_text_regions tr
     WHERE tr.source_id = t.source_id
       AND tr.source = t.source
       AND tr.hazard = t.hazard
-      AND NOT deleted
-      AND t.source_timestamp >= @maxTimestamp;
+      AND t.source_timestamp >= @maxTimestamp - INTERVAL 4 DAY;
     COMMIT;
 
     START TRANSACTION;
 
     SET @maxTimestampTSD = IFNULL((select max(source_date) from mat_view_timeseries_date), NOW() - INTERVAL 20 YEAR);
     REPLACE INTO mat_view_timeseries_date
-    SELECT rrg.parent     as region_group_name,
-           t.source       as source,
-           t.hazard       as hazard,
-           t.warning      as warning,
-           t.source_date  as source_date,
-           t.source_text  as source_text,
-           vr.region_type as region_type
+    SELECT rrg.parent               as region_group_name,
+           t.source                 as source,
+           t.hazard                 as hazard,
+           t.warning                as warning,
+           t.source_date            as source_date,
+           t.source_text            as source_text,
+           vr.region_type           as region_type,
+           IFNULL(t.deleted, false) as deleted
     FROM live_text_regions vr,
          live_text t,
          ref_region_groups as rrg
@@ -54,7 +55,7 @@ BEGIN
       AND t.source_id = vr.source_id
       AND t.source = vr.source
       AND t.hazard = vr.hazard
-      AND t.source_date >= @maxTimestampTSD;
+      AND t.source_date >= @maxTimestampTSD - INTERVAL 4 DAY;
     COMMIT;
 
     SET rc = 0;
