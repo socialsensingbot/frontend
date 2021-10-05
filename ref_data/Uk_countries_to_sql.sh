311be1b4e0938fad2d1fc23a22e10c9eed0c47e0
neilellis <hello@neilellis.me>
2021-10-05 19:17:40 +0100

    PWS - 3.2c - View most recent UK tweets in a list #250

--- ref_data/Uk_countries_to_sql.sh
+++ ref_data/Uk_countries_to_sql.sh
@@ -1,16 +1,14 @@
-
 process() {
 
-while read x
-do
+  while read x; do
 
-  geo=$( cat $1 | jq --arg locid "$x" -c '.features[] | select(.properties.id == $locid) | .geometry' | sed "s/\n//g")
-  region=$x
-  title=$( cat $1 | jq --arg locid "$x" -c '.features[] | select(.properties.id == $locid) | .properties.name' | sed "s/\n//g")
-  echo "replace into ref_geo_regions (map_location,region_type,region,boundary,title) VALUES ('uk','countries','$region', ST_GeomFromGeoJSON('$geo'),'$title');"
+    geo=$(cat $1 | jq --arg locid "$x" -c '.features[] | select(.properties.id == $locid) | .geometry' | sed "s/\n//g")
+    region=$x
+    title=$(cat $1 | jq --arg locid "$x" -c '.features[] | select(.properties.id == $locid) | .properties.name' | sed "s/\n//g")
+    echo "replace into ref_geo_regions (map_location,region_type,region,boundary,title) VALUES ('uk','countries','$region', ST_GeomFromGeoJSON('$geo'),'$title');"
 
-done
+  done
 
 }
 
-cat uk_countries.geojson |  jq -cr ".features[].properties.id" | process uk_countries.geojson > uk_countries.sql
+cat uk_countries.geojson | jq -cr ".features[].properties.id" | process uk_countries.geojson >uk_countries.sql
