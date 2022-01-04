68a1d18ed10e5106ce922de547d09b59e2471ac4
neilellis <hello@neilellis.me>
2022-01-04 17:09:19 +0000

    Test fixes.

--- cypress/integration/00_quick/04_map_spec.js
+++ cypress/integration/00_quick/04_map_spec.js
@@ -59,7 +59,7 @@ describe('04: Map: ', function () {
             cy.get(".app-map-expand-toolbar-btn").should("be.visible").click();
             cy.wait(1000);
             cy.get('.app-map-number-layer-select').click()
-            cy.get('#mat-option-13 > .mat-option-text').contains('Exceedance').click();
+            cy.get('.app-map-nls-exceedance > .mat-option-text').contains('Exceedance').click();
 
             cy.get(legendEntry).get("i").should("have.attr", "style").should("contain", statsFirstLegendColour)
             cy.get(legendEntry).should("have.text", statsFirstLegendVal);
@@ -74,6 +74,7 @@ describe('04: Map: ', function () {
             cy.twitterPanelVisible();
             cy.get(".app-draw-close-button > .mat-button-wrapper > .mat-icon", {timeout: 30000}).should("be.visible");
             cy.get(".app-draw-close-button > .mat-button-wrapper > .mat-icon", {timeout: 30000}).click();
+            cy.wait(10000);
             cy.twitterPanelNotVisible();
             cy.logout();
         });
