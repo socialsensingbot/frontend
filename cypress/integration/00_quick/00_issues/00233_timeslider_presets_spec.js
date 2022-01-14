324296d28aa24314e53627efb2867da4b2222bdd
neilellis <hello@neilellis.me>
2022-01-14 19:02:55 +0000

    Fix for problems with :-
    FFC: Add "today" and "yesterday" as options in the time window menu? #376

--- cypress/integration/00_quick/00_issues/00233_timeslider_presets_spec.js
+++ cypress/integration/00_quick/00_issues/00233_timeslider_presets_spec.js
@@ -37,7 +37,7 @@ describe('#233 Timeslide presets: : https://github.com/socialsensingbot/frontend
             cy.log("Checking for Drop down time range issues. #275")
             cy.get(".app-tweet-drawer", {timeout: 60000}).should("be.visible");
             cy.url().should("equal",
-                            MAP_URL + "?zoom=5&max_time=1631660399999&min_time=1631574000000&active_number=exceedance&active_polygon=county&selected=greater%20london");
+                            MAP_URL + "?zoom=5&max_time=1631660400000&min_time=1631574000000&active_number=exceedance&active_polygon=county&selected=greater%20london");
             cy.get('#mat-tab-label-1-0').should("have.text", "168 Tweets");
             cy.logout();
         });
