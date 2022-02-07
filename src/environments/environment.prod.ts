daee2d4ce19021b26a8cae1cc6a33ed147b94a79
neilellis <hello@neilellis.me>
2022-02-07 15:12:48 +0000

    Branch for Release 2.5.1 created.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -32,7 +32,7 @@ export const environment = {
     name:             "production",
     lamdaEnvironment: "prod",
     demo:             false,
-    version:          "2.5",
+    version: "2.5.1",
     // timezone:Intl.DateTimeFormat().resolvedOptions().timeZone,
     timezone:             "UTC",
     locale:               "en-GB",
