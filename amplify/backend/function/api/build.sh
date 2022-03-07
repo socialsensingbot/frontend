a19ce98a68dc2447a669b9e900d4a4fa3926cb7f
neilellis <hello@neilellis.me>
2022-03-07 19:54:31 +0000

    Initial work on API support.

--- amplify/backend/function/api/build.sh
+++ amplify/backend/function/api/build.sh
@@ -1,8 +1,9 @@
 cd ../../../../api || exit
-npm install
+npm ci
+tsc --project tsconfig.json
 npm pack
 cd - || exit
 cd src || exit
 npm install ../../../../../api/socialsensing-api*.tgz
-npm install
+npm ci
 tsc --project tsconfig.json
