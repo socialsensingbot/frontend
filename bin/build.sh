ef780c032ec37acb40dab8519cf16d5bd85adb7a
neilellis <hello@neilellis.me>
2022-03-08 18:02:07 +0000

    Build fixes.

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,8 @@
 cd $(dirname $0)
 cd ..
 cp src/app/map/data/map-data.ts api/
+./amplify/backend/function/query/build.sh
+./amplify/backend/function/api/build.sh
 
 npx browserslist@latest --update-db
 
