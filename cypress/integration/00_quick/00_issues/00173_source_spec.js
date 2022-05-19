e9ef8568ac7cfbd1d109b4c1b171d3cf8e0b0405
neilellis <hello@neilellis.me>
2022-05-19 20:16:29 +0100

    Fixing tests for release 2.7.

--- cypress/integration/00_quick/00_issues/00173_source_spec.js
+++ cypress/integration/00_quick/00_issues/00173_source_spec.js
@@ -37,13 +37,11 @@ describe('00173 Source: : https://github.com/socialsensingbot/frontend/issues/17
       clickSourceMenu();
       cy.get("body .tweet-list-item-menu-source-clear", {timeout: 30000}).click();
       clickSourceMenu();
-      cy.get("body .tweet-list-item-menu-source-level-coastal-not-selected", {timeout: 30000});
-      cy.get("body .tweet-list-item-menu-source-level-coastal", {timeout: 30000}).click();
+      cy.get("body .tweet-list-item-menu-source-level-coastal-not-selected", {timeout: 30000}).click();
       cy.wait(10000);
       clickSourceMenu();
       cy.get("body .tweet-list-item-menu-source-level-coastal-selected", {timeout: 30000});
-      cy.get("body .tweet-list-item-menu-source-level-river-not-selected", {timeout: 30000});
-      cy.get("body .tweet-list-item-menu-source-level-river", {timeout: 30000}).click();
+      cy.get("body .tweet-list-item-menu-source-level-river-not-selected", {timeout: 30000}).click();
       cy.wait(10000);
       clickSourceMenu();
       cy.get("body .tweet-list-item-menu-source-level-coastal-not-selected", {timeout: 30000});
