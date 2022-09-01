e10b68d8bf6f8f58ae6214829d6c259f021d787d
neilellis <hello@neilellis.me>
2022-09-01 16:04:08 +0100

    Fixing tests for release/2.7.1

--- cypress/e2e/00_quick/03_ignore_pref_spec.js
+++ cypress/e2e/00_quick/03_ignore_pref_spec.js
@@ -18,9 +18,9 @@ describe('03 Ignore tweets: ', function () {
         cy.login();
         cy.visitAndWait(url);
         cy.get(".app-tweet-drawer", {timeout: 30000}).should("be.visible");
+        cy.wait(20000);
         cy.get(".app-tweet-table", {timeout: 30000}).then(panel => {
-            cy.wait(30000);
-            // Make sure other tests have not hidden the tweet we're testing against
+            cy.log(panel);
             if (panel.find(POWYS_TWEET).length > 0) {
                 cy.get(POWYS_TWEET_VISIBLE, {timeout: 60000});
                 cy.get(POWYS_TWEET_VISIBLE, {timeout: 60000}).should('be.visible');
